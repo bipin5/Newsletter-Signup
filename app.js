7fe24036649b2a3feaad19a8422bada733d88462
Bipin Dahal <bpn11dahal@gmail.com>
2021-11-01 13:13:09 +0545

    Initial Commit

---
+++ app.js
@@ -0,0 +1,29 @@
+//jshint esversion: 6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var email = req.body.email;
+
+  console.log(firstName, lastName, email);
+
+  // request("", function(error,response,body){
+  // });
+});
+
+app.listen(3000, function(req, res){
+  console.log("Server is listening on port 3000");
+});
